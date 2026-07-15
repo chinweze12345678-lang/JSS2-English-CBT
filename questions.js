45f29e55325a02b458e9fbdc5d7401ec80a874be
chinweze12345678-lang <chinweze12345678@gmail.com>
2026-07-15 22:21:27 +0100

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,34 @@
+const questions = [
+  {
+    question: "Which sentence is in the Future Continuous Tense?",
+    options: [
+      "I eat.",
+      "I ate.",
+      "I will be eating.",
+      "I have eaten."
+    ],
+    answer: 2
+  },
+
+  {
+    question: "Which of these is an adjective?",
+    options: [
+      "Quickly",
+      "Beautiful",
+      "Happily",
+      "Slowly"
+    ],
+    answer: 1
+  },
+
+  {
+    question: "Choose the correct question tag: She is a nurse, _____?",
+    options: [
+      "isn't she",
+      "doesn't she",
+      "don't she",
+      "wasn't she"
+    ],
+    answer: 0
+  }
+];
